fdd48e104fcf0d25126eff135b2d0a0c59e99508
Miloga <catdiscord8@gmail.com>
2026-02-25 15:45:39 +0200

    Delete app directory

--- app/db/database.py
+++
@@ -1,5 +0,0 @@
-from supabase import create_client, Client
-from config import SUPABASE_URL, SUPABASE_KEY
-# TODO: MAKE THE DAMN FILE IMPORTS WORK BECAUSE I DONT KNOW HOW
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
